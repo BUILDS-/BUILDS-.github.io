5124e5faba7ebae79c1dab9d7cc36467db01cca0
Phillip Tran <ptrandev@protonmail.com>
2022-09-11 18:12:56 -0400

    addressed lint warnings

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,27 +1,5 @@
 import React from "react";
 // import PropTypes from 'prop-types'
-import styled from "styled-components";
-import Button from "../components/UI/Button";
-
-const H3 = styled.h3`
-  font-size: 2em;
-  margin: 0;
-  padding: 0.5em 0;
-`;
-
-const Card = styled.div`
-  background-color: rgba(0, 0, 0, 0.1);
-  border-radius: 20px;
-  h4 {
-    margin: 0;
-    font-weight: 100;
-    font-size: 2em;
-  }
-  .content {
-    padding: 1em;
-  }
-`;
-
 
 const Calendar = (props) => {
   return (
